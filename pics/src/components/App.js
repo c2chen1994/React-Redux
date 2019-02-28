67c6f3b62959423b8e7b36374ab02f3c17e0e6ca
Zhuo Chen <chen519@usc.edu>
2019-02-28 12:18:23 -0800

    then and async await

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,15 +3,20 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get("https://api.unsplash.com/search/photos", {
+  async onSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
         Authorization:
           "Client-ID 31fecb1fc173093b3f31111e5e169498e1c061bc3fbea826161f1e1718a786ca"
       }
     });
-    console.log(term);
+
+    //   .then(response => {
+    //     console.log(response.data.results);
+    //   });
+
+    console.log(response.data.results);
   }
 
   render() {
