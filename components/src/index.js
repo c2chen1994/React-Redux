172ca238ac27212a51f90b1b6f0abe482016de05
Zhuo Chen <chen519@usc.edu>
2019-02-27 16:42:44 -0800

    component nesting

--- components/src/index.js
+++ components/src/index.js
@@ -1,29 +1,22 @@
-// Import React and ReactDOM
 import React from "react";
 import ReactDOM from "react-dom";
-import faker from "faker";
+import CommentDatail from "./CommentDetail";
+const comments = () => {
+  return <div className="ui container comments">{comment}</div>;
+};
 
-// Create a react component
+const comment = () => {};
+
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00 PM</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
+      <CommentDatail />
+      <CommentDatail />
+      <CommentDatail />
+      <CommentDatail />
+      <CommentDatail />
     </div>
   );
 };
 
-// Take the react component and show it on the screen
 ReactDOM.render(<App />, document.querySelector("#root"));
