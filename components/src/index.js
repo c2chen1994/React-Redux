d91e2ffe3f92d37f5c2119286d6d9ecec1f7bbd1
Zhuo Chen <chen519@usc.edu>
2019-02-27 16:49:37 -0800

    passing and receiving props

--- components/src/index.js
+++ components/src/index.js
@@ -1,20 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentDatail from "./CommentDetail";
-const comments = () => {
-  return <div className="ui container comments">{comment}</div>;
-};
-
-const comment = () => {};
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDatail />
-      <CommentDatail />
-      <CommentDatail />
-      <CommentDatail />
-      <CommentDatail />
+      <CommentDatail author="Sam" />
+      <CommentDatail author="Jack" />
+      <CommentDatail author="Tony" />
+      <CommentDatail author="Tom" />
+      <CommentDatail author="Zack" />
     </div>
   );
 };
