454450e15868d40aea99c16f77631a0961c5b486
Zhuo Chen <chen519@usc.edu>
2019-02-27 16:15:03 -0800

    semantic-ui.css

---
+++ components/src/index.js
@@ -0,0 +1,11 @@
+// Import React and ReactDOM
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Create a react component
+const App = () => {
+  return <div>Hi there!</div>;
+};
+
+// Take the react component and show it on the screen
+ReactDOM.render(<App />, document.querySelector("#root"));
