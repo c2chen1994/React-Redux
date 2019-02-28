6c2eb836c8bb5da34c48aec2d7fe84d908d9acc7
Zhuo Chen <chen519@usc.edu>
2019-02-27 17:14:01 -0800

    component reuse and children

--- components/src/index.js
+++ components/src/index.js
@@ -2,40 +2,57 @@ import React from "react";
 import ReactDOM from "react-dom";
 import CommentDatail from "./CommentDetail";
 import faker from "faker";
+import ApprovalCard from "./AprrovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDatail
-        author="Sam"
-        timeAgo="Today at 4:45 PM"
-        content="Nice blog post 1"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDatail
-        author="Jack"
-        timeAgo="Today at 9:25 AM"
-        content="Nice blog post 2"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDatail
-        author="Tony"
-        timeAgo="Yesterday at 2:00 PM"
-        content="Nice blog post 3"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDatail
-        author="Tom"
-        timeAgo="Today at 8:30 PM"
-        content="Nice blog post 4"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDatail
-        author="Zack"
-        timeAgo="Yesterday at 10:50 AM"
-        content="Nice blog post 5"
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure???
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDatail
+          author="Sam"
+          timeAgo="Today at 4:45 PM"
+          content="Nice blog post 1"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDatail
+          author="Jack"
+          timeAgo="Today at 9:25 AM"
+          content="Nice blog post 2"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDatail
+          author="Tony"
+          timeAgo="Yesterday at 2:00 PM"
+          content="Nice blog post 3"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDatail
+          author="Tom"
+          timeAgo="Today at 8:30 PM"
+          content="Nice blog post 4"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDatail
+          author="Zack"
+          timeAgo="Yesterday at 10:50 AM"
+          content="Nice blog post 5"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
