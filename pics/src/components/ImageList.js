5e99aa2f6f9b00b93e48a97461fe508fbd2d47d7
Zhuo Chen <chen519@usc.edu>
2019-02-28 12:51:49 -0800

    keys in list

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -3,9 +3,9 @@ import React from "react";
 const ImageList = props => {
   const images = props.images.map(image => {
     return (
-      <li key={image.id}>
-        <img src={image.urls.regular} />
-      </li>
+      <div key={image.id}>
+        <img src={image.urls.regular} alt={image.description} />
+      </div>
     );
   });
   return <div>{images}</div>;
