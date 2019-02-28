112cb2026448eaf25f2f14d3306757d3845d9412
Zhuo Chen <chen519@usc.edu>
2019-02-27 21:11:19 -0800

    lifecycle methods

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -16,6 +16,15 @@ class App extends React.Component {
       }
     );
   }
+
+  componentDidMount() {
+    console.log("My component was rendered to the screen");
+  }
+
+  componentDidUpdate() {
+    console.log("My component was just updated - it rerendered!");
+  }
+
   render() {
     if (this.state.lat) return <div>Latitude: {this.state.lat}</div>;
     if (this.state.errMessage) return <div>Error:{this.state.errMessage}</div>;
