a64038a6b309ae365f6f61cd1987a6b2e8dd5e7d
Zhuo Chen <chen519@usc.edu>
2019-02-28 13:11:59 -0800

    image card

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,14 +1,11 @@
 import "./ImageList.css";
 import React from "react";
+import ImageCard from "./ImageCard";
 
 const ImageList = props => {
-  const images = props.images.map(image => {
-    return (
-      <div key={image.id}>
-        <img src={image.urls.regular} alt={image.description} />
-      </div>
-    );
-  });
+  const images = props.images.map(image => (
+    <ImageCard key={image.id} image={image} />
+  ));
   return <div className="image-list">{images}</div>;
 };
 
