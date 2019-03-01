a52d51c1128bc02a167982d05f90f88ed3ea3a33
Zhuo Chen <chen519@usc.edu>
2019-02-28 20:18:38 -0800

    Provider and connect action creators and producers

---
+++ songs/src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SongList from "./SongList";
+
+const App = () => {
+  return (
+    <div className="ui container" style={{ marginTop: "20px" }}>
+      <SongList />
+    </div>
+  );
+};
+
+export default App;
