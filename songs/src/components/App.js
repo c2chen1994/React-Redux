805fa48437528a51551e57c01ac9ea169890063e
Zhuo Chen <chen519@usc.edu>
2019-02-28 20:50:33 -0800

    rendering song list and details

--- songs/src/components/App.js
+++ songs/src/components/App.js
@@ -1,10 +1,18 @@
 import React from "react";
 import SongList from "./SongList";
+import SongDetail from "./SongDetail";
 
 const App = () => {
   return (
-    <div className="ui container" style={{ marginTop: "20px" }}>
-      <SongList />
+    <div className="ui container grid" style={{ marginTop: "20px" }}>
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
+        </div>
+        <div className="column eight wide">
+          <SongDetail />
+        </div>
+      </div>
     </div>
   );
 };
