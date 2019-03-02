7f5e7568f9c477f5086fbddce3f8da8c5014ee24
Zhuo Chen <chen519@usc.edu>
2019-03-02 12:08:50 -0800

    form field and submit

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,7 +1,40 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
+//import
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
+class StreamCreate extends React.Component {
+  renderInput({ input, label }) {
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} />
+      </div>
+    );
+  }
 
-export default StreamCreate;
+  onSumbit(formValues) {
+    // preventDefault: redux-form does for us
+    console.log(formValues);
+  }
+
+  render() {
+    return (
+      <form
+        className="ui form"
+        onSubmit={this.props.handleSubmit(this.onSumbit)}
+      >
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter Description"
+        />
+        <button className="ui button primary">Submit</button>
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: "streamCreate"
+})(StreamCreate);
