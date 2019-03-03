2e3fa30f1c411924c07c583ad29479eb59b80404
Zhuo Chen <chen519@usc.edu>
2019-03-03 12:25:54 -0800

    Updates

---
+++ precache-manifest.9db57403d50cdba44f8cd4c3ba70ccc2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e2d1fb7bb9a56411fff",
+    "url": "/c2chen1994/React-Redux/tree/master/videos/static/css/main.d99912b2.chunk.css"
+  },
+  {
+    "revision": "9e2d1fb7bb9a56411fff",
+    "url": "/c2chen1994/React-Redux/tree/master/videos/static/js/main.9e2d1fb7.chunk.js"
+  },
+  {
+    "revision": "91d17d4ebba8d69d2358",
+    "url": "/c2chen1994/React-Redux/tree/master/videos/static/js/runtime~main.91d17d4e.js"
+  },
+  {
+    "revision": "cb016095e848669d37ac",
+    "url": "/c2chen1994/React-Redux/tree/master/videos/static/js/2.cb016095.chunk.js"
+  },
+  {
+    "revision": "6623dd9fa375ebb13d37ad61f6e980e6",
+    "url": "/c2chen1994/React-Redux/tree/master/videos/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/c2chen1994/React-Redux/tree/master/videos/precache-manifest.9db57403d50cdba44f8cd4c3ba70ccc2.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/c2chen1994/React-Redux/tree/master/videos/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
