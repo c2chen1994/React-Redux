40b74a054af950d70fcb423410ef48ef039ec2da
Zhuo Chen <chen519@usc.edu>
2019-03-03 12:53:50 -0800

    Updates

---
+++ precache-manifest.2c874e82301bc9f8404731a6b68a5ea0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e2d1fb7bb9a56411fff",
+    "url": "/React-Redux/static/css/main.d99912b2.chunk.css"
+  },
+  {
+    "revision": "9e2d1fb7bb9a56411fff",
+    "url": "/React-Redux/static/js/main.9e2d1fb7.chunk.js"
+  },
+  {
+    "revision": "b750563d87c4a35f96a3",
+    "url": "/React-Redux/static/js/runtime~main.b750563d.js"
+  },
+  {
+    "revision": "cb016095e848669d37ac",
+    "url": "/React-Redux/static/js/2.cb016095.chunk.js"
+  },
+  {
+    "revision": "aa6cf8816053210b7e3f92e98a3befb5",
+    "url": "/React-Redux/index.html"
+  }
+];
