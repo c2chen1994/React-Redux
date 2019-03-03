1484c34cca6e66674c992b37205f0596b37e9684
Zhuo Chen <chen519@usc.edu>
2019-03-03 12:08:17 -0800

    reuse useLocation function

---
+++ seasons-hooks/src/useLocation.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+
+export default () => {
+  const [lat, setLat] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    window.navigator.geolocation.getCurrentPosition(
+      position => setLat(position.coords.latitude),
+      err => setErrorMessage(err.message)
+    );
+  }, []); // [] only one time in total
+
+  return [lat, errorMessage];
+};
